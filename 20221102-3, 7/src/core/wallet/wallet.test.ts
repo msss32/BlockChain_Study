a01c4e00dcb22699f0984811fb63eadff7f63054
msss32 <msss14@naver.com>
2022-11-08 12:35:57 +0900

    block-chain

--- 20221102-3, 7/src/core/wallet/wallet.test.ts
+++ 20221102-3, 7/src/core/wallet/wallet.test.ts
@@ -48,3 +48,93 @@
 // 3. 서명이랑 공개키를 제 3자에게 전달
 // 4. 제 3자는 공개키를 이용해서 서명을 복호화
 // 5. 복호화한 해시값과 데이터를 해싱해서 나온 값이 맞는지 확인
+
+// 개인키 생성 테스트
+
+// 실제 블록체인 네트워크 상에 개인키를 생성하는 방식은
+// 256자리의 2진수로 랜덤값을 64자리의 16진수 값으로 만든 것이 개인키
+
+// 개인키 작성
+
+import { randomBytes } from "crypto";
+import elliptic from "elliptic";
+import { SHA256 } from "crypto-js";
+
+// elliptic 인스턴스 생성
+const ec = new elliptic.ec("secp256k1");
+
+describe("지갑 만들기", () => {
+  let privateKey: string;
+  let publicKey: string;
+  let signature: elliptic.ec.Signature;
+
+  it("개인키 생성", () => {
+    // 256자리의 2진수 랜덤 값을 만들고 toString("hex")를 사용해서
+    // 16진수로 나타내줌
+    privateKey = randomBytes(32).toString("hex");
+    console.log("개인키 : " + privateKey);
+    console.log("길이 : " + privateKey.length);
+  });
+
+  it("공개키 생성", () => {
+    const keyPair = ec.keyFromPrivate(privateKey);
+    publicKey = keyPair.getPublic().encode("hex", true);
+    console.log("공개키 : ", publicKey);
+    console.log("길이 : " + publicKey.length);
+  });
+
+  it("서명 만들기", () => {
+    // 개인키랑 hash 값이 필요해서 SHA256 함수 사용
+    const keyPair = ec.keyFromPrivate(privateKey);
+    const hash = SHA256("transaction data").toString();
+    signature = keyPair.sign(hash, "hex");
+    console.log("서명 : ", signature);
+  });
+
+  // 마지막 검증하기
+  // 해시값, 서명, 공개키
+  // 공개키를 사용해서 서명을 복호화하고 나온 값이
+  // 해시와 동일하면 서명은 공개키를 생성한 소유자에 의해 만들어진 서명인 것이 증명됨
+
+  it("검증하기", () => {
+    // 필요한 값 : 서명, 공개키, hash
+    const hash = SHA256("transaction data").toString();
+    const verify = ec.verify(
+      hash,
+      signature,
+      ec.keyFromPublic(publicKey, "hex")
+    );
+    console.log(verify);
+  });
+
+  // 계정 만들기
+  it("지갑 주소", () => {
+    // 버퍼는 raw바이너리 데이터를 저장할 수 있는 특수한 유형의 객체
+    // 버퍼는 일반적으로 PC에 할당된 메모리 청크 RAM을 나타내고
+    // 버퍼의 크기를 설정하면 변경할 수 없음
+
+    // Buffer.from은 버퍼를 만드는 방법
+    // string 배열을 인수로 받고 넘겨주는 값에 따라 약간씩 다른 방법으로 만듦
+    const buffer = Buffer.from(publicKey);
+    // 이더리움 방식으로 계정을 만드는 방법은 만들어놓은 공개키에서
+    // 앞의 24자리를 잘라내고 40자리만큼 남겨줌
+    // slice(26)을 넣은 이유가 elliptic을 이용해서 만든
+    // 공개키의 앞자리 02 혹은 03이 붙어서 이 값을 제거하기 위함
+    const address = buffer.slice(26).toString();
+    console.log("계정 : " + address);
+  });
+  // 동일한 내용을 "transaction data" 값으로 해시값을 만들어서 검증에 사용한 것이기 때문에
+  // 검증에 성공한 것 true 반환
+
+  // 개인키랑 페어를 이루는 공개키 만들기
+  // elliptic라는 라이브러리를 사용
+  // 실제 개인키를 사용해서 공개키를 만드는 알고리즘에 타원곡선 함수라는 수학적 개념 포함
+  // 타원 곡선 알고리즘을 직접 구현하는 건 어려우니까
+  // 개인키를 사용해서 공개키를 쉽게 만들 수 있게 함수를 제공해주는 라이브러리를 사용
+  // 설치 명령어
+  // npm i elliptic
+  // npm i -D @types/elliptic
+
+  // 블록체인 지갑의 서버
+  // 지갑 프로그램(클라이언트) -> 지갑 서버(서버) -> 블록체인 http server -> 블록체인 P2P 네트워크
+});
